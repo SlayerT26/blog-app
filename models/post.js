113349744aeb016ac47273b3e49cca8810f34b32
SlayerT26 <456theochang@gmail.com>
2021-09-03 11:30:49 -0700

    data/post

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const Blog = new Schema(
     title: { type: String, required: true },
     image_url: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: String, required: true }
+    // price: { type: String, required: true }
     // github_url: { type: String, required: true },
     // deployed_url: { type: String, required: true }
   },
---
+++ seed/data.js
@@ -0,0 +1,33 @@
+import db from '../db/connection.js'
+import Blog from '../models/post.js'
+
+const insertData = async () => {
+  // reset database
+  await db.dropDatabase()
+
+  // create an array of 100 objects
+  // use faker package to generate fake data
+  const blogs = [
+    {
+      title: 'Blog 001',
+      image_url:
+        'https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      title: 'Blog 002',
+      image_url:
+        'https://images.unsplash.com/photo-1576594496020-534bf2437b21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    }
+  ]
+  await Blog.insertMany(blogs)
+  console.log('Created blog post!')
+
+  // close database connection. done.
+  db.close()
+}
+
+insertData()
