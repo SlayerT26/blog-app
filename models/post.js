e266065825c89a78991788cf1f423ffeb8e4cd30
SlayerT26 <456theochang@gmail.com>
2021-09-03 11:39:43 -0700

    changed blog to post

--- models/post.js
+++ models/post.js
@@ -1,16 +1,13 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
-const Blog = new Schema(
+const Post = new Schema(
   {
     title: { type: String, required: true },
     image_url: { type: String, required: true },
     description: { type: String, required: true },
-    // price: { type: String, required: true }
-    // github_url: { type: String, required: true },
-    // deployed_url: { type: String, required: true }
   },
   { timestamps: true },
 )
 
-export default mongoose.model('blogs', Blog)
+export default mongoose.model('posts', Post)
--- seed/data.js
+++ seed/data.js
@@ -1,5 +1,5 @@
 import db from '../db/connection.js'
-import Blog from '../models/post.js'
+import Post from '../models/post.js'
 
 const insertData = async () => {
   // reset database
@@ -7,24 +7,24 @@ const insertData = async () => {
 
   // create an array of 100 objects
   // use faker package to generate fake data
-  const blogs = [
+  const posts = [
     {
-      title: 'Blog 001',
+      title: 'Post 001',
       image_url:
         'https://images.unsplash.com/photo-1573521193826-58c7dc2e13e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
     {
-      title: 'Blog 002',
+      title: 'Post 002',
       image_url:
         'https://images.unsplash.com/photo-1576594496020-534bf2437b21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     }
   ]
-  await Blog.insertMany(blogs)
-  console.log('Created blog post!')
+  await Post.insertMany(posts)
+  console.log('Created post!')
 
   // close database connection. done.
   db.close()
